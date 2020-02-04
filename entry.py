2a2d6488e3c1530032d2c80d370ecc905f9a59b5
leontev <andruwwwka@gmail.com>
2020-02-04 23:57:53 +0300

    test heroku deploy

--- entry.py
+++ entry.py
@@ -11,6 +11,7 @@ from calculator.settings import load_config
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 parser = argparse.ArgumentParser(description='Calculator for Tomas')
+parser.add_argument('--port', help='Port to accept connections', default=8080)
 parser.add_argument(
     '-c',
     '--config',
@@ -22,4 +23,4 @@ args = parser.parse_args()
 app = create_app(config=load_config(args.config))
 
 if __name__ == '__main__':
-    aiohttp.web.run_app(app)
+    aiohttp.web.run_app(app, port=args.port)
